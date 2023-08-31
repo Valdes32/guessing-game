509688c316a1e6fa4ba47b9b090aaefcb4db0f2a
Valdes32 <pro100kul@gmail.com>
2023-08-31 12:01:45 +0300

    feat:class GuessingGame

--- index.js
+++ index.js
@@ -1,3 +1,2 @@
-const GuessingGame = require('./src/guessing-game.js');
-
-window.game = new GuessingGame();
+const game = new GuessingGame();
+window.game = game;
--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,28 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.minRange = 0;
+    this.maxRange = 0;
+    this.currentGuess = 0;
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.minRange = min;
+    this.maxRange = max;
+  }
 
-    }
-
-    guess() {
-
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+	guess() {
+		this.currentGuess = Math.ceil((this.minRange + this.maxRange) / 2);
+  return this.currentGuess;
 }
+
+  lower() {
+    this.maxRange = this.currentGuess;
+  }
+
+  greater() {
+    this.minRange = this.currentGuess;
+  }
+}
+
 
 module.exports = GuessingGame;
